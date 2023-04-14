31a1aee2a537957236711fab7069b6170de72a60
zhu jing yang <3161362058@qq.com>
2023-04-14 17:27:02 +0800

    fix cors

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   // output: 'export',
-  // async rewrites () {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'https://zbvrsg.laf.dev/:path*'
-  //     }
-  //   ]
-  // }
+  async rewrites () {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://zbvrsg.laf.dev/:path*'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { Box } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
-
 export default function Index(props: any) {
-  const router = useRouter()
-
-  // useEffect(() => {
-  //   router.replace('/community')
-  // }, [router])
-
   return props.children
 }
